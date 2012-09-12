b4644e6d4c6d099bb6fb7f867256f3ac6f2a2fbe
lching <lching@lching.(none)>
2012-09-12 15:47:47 +0800

    Added AccountHolder and BankUser models.

---
+++ src/main/java/com/safegard/bank/model/AccountHolder.java
@@ -0,0 +1,73 @@
+package com.safegard.bank.model;
+
+import java.util.Date;
+
+import com.safegard.bank.enums.Gender;
+
+public class AccountHolder {
+
+    private String lastName;
+    private String firstName;
+    private String middleName;
+    private Date birthdate;
+    private Gender gender;
+    private String address;
+    private String phone;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
