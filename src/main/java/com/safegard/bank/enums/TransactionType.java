8eb8bed17592380361fd8e6eaf605b54595755d9
Monte Cillo Co <mco@exist.com>
2012-09-12 16:57:14 +0800

    Add Account and Transaction Table.

---
+++ src/main/java/com/safegard/bank/enums/TransactionType.java
@@ -0,0 +1,13 @@
+package com.safegard.bank.enums;
+
+/**
+ *
+ * @author Monte Cillo Co<mco@exist.com>
+ */
+public enum TransactionType {
+
+	DEPOSIT, 
+	WITHDRAWAL, 
+	TRANSFER, 
+	INQUIRY
+}
--- src/main/java/com/safegard/bank/model/AccountHolder.java
+++ src/main/java/com/safegard/bank/model/AccountHolder.java
@@ -1,82 +1,96 @@
 package com.safegard.bank.model;
 
 import java.util.Date;
-
 import javax.persistence.Column;
+import javax.persistence.OneToOne;
+import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
+
+@Entity
 @Table(name = "ACCOUNT_HOLDER")
+@XmlRootElement
 public class AccountHolder {
 
-    private String lastName;
-    private String firstName;
-    private String middleName;
-    private Date birthdate;
-    private String gender;
-    private String address;
-    private String phone;
+	private String lastName;
+	private String firstName;
+	private String middleName;
+	private Date birthdate;
+	private String gender;
+	private String address;
+	private String phone;
+	private Account account;
 
-    @Column(name = "LAST_NAME", length = 25)
-    public String getLastName() {
-        return lastName;
-    }
+	@Column(name = "LAST_NAME", length = 25)
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    @Column(name = "FIRST_NAME", length = 25)
-    public String getFirstName() {
-        return firstName;
-    }
+	@Column(name = "FIRST_NAME", length = 25)
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    @Column(name = "MIDDLE_NAME", length = 25)
-    public String getMiddleName() {
-        return middleName;
-    }
+	@Column(name = "MIDDLE_NAME", length = 25)
+	public String getMiddleName() {
+		return middleName;
+	}
 
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
 
-    @Column(name = "BIRTHDATE")
-    public Date getBirthdate() {
-        return birthdate;
-    }
+	@Column(name = "BIRTHDATE")
+	public Date getBirthdate() {
+		return birthdate;
+	}
 
-    public void setBirthdate(Date birthdate) {
-        this.birthdate = birthdate;
-    }
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
 
-    @Column(name = "GENDER")
-    public String getGender() {
-        return gender;
-    }
+	@Column(name = "GENDER")
+	public String getGender() {
+		return gender;
+	}
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
-    @Column(name = "ADDRESS")
-    public String getAddress() {
-        return address;
-    }
+	@Column(name = "ADDRESS")
+	public String getAddress() {
+		return address;
+	}
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-    @Column(name = "PHONE")
-    public String getPhone() {
-        return phone;
-    }
+	@Column(name = "PHONE")
+	public String getPhone() {
+		return phone;
+	}
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
+	@OneToOne(optional = false, mappedBy = "accountHolder")
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
 }
