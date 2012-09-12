e314634f89f256e47e6f3190a3504ff0af5e1c55
Monte Cillo Co <mco@exist.com>
2012-09-12 16:57:14 +0800

    Add Account and Transaction Table.

--- src/main/java/com/safegard/bank/model/Account.java
+++ src/main/java/com/safegard/bank/model/Account.java
@@ -1,10 +1,110 @@
-
 package com.safegard.bank.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+import javax.persistence.*;
 
 /**
  *
  * @author Monte Cillo Co<mco@exist.com>
  */
-public class Account {
+@Entity
+@Table(name = "ACCOUNT")
+public class Account implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Version
+	private int version;
+	@Column(name = "ACCNT_NO")
+	private String accountNo;
+	@OneToOne
+	private AccountHolder accountHolder;
+	@Column(name = "PIN")
+	private String pin;
+	@Column(name = "REG_DATE")
+	@Temporal(javax.persistence.TemporalType.DATE)
+	private Date reg_date;
+	@Column(name = "AVAILABLE_BAL")
+	private double availableBalance;
+	@Column(name = "TOTAL_BAL")
+	private double totalBalance;
+	@OneToMany(
+            mappedBy = "TRANS_RECORD",
+			   cascade = CascadeType.ALL)
+	private Set<TransactionRecord> transactionRecords;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+
+	public Date getReg_date() {
+		return reg_date;
+	}
+
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+
+	public double getAvailableBalance() {
+		return availableBalance;
+	}
+
+	public void setAvailableBalance(double availableBalance) {
+		this.availableBalance = availableBalance;
+	}
+
+	public double getTotalBalance() {
+		return totalBalance;
+	}
+
+	public void setTotalBalance(double totalBalance) {
+		this.totalBalance = totalBalance;
+	}
+
+	public AccountHolder getAccountHolder() {
+		return accountHolder;
+	}
+
+	public void setAccountHolder(AccountHolder accountHolder) {
+		this.accountHolder = accountHolder;
+	}
 	
+	public Set<TransactionRecord> getTransactionRecords() {
+		return transactionRecords;
+	}
+
+	public void setTransactionRecords(Set<TransactionRecord> transactionRecords) {
+		this.transactionRecords = transactionRecords;
+	}
 }
---
+++ src/main/java/com/safegard/bank/model/TransactionRecord.java
@@ -0,0 +1,70 @@
+package com.safegard.bank.model;
+
+import com.safegard.bank.enums.TransactionType;
+import java.io.Serializable;
+import javax.persistence.*;
+import org.joda.time.DateTime;
+
+/**
+ *
+ * @author Monte Cillo Co<mco@exist.com>
+ */
+@Entity
+@Table(name = "TRANS_RECORD")
+public class TransactionRecord implements Serializable {
+
+	@Id
+	private Long id;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "TYPE", length = 20)
+	private TransactionType type;
+	@Column(name = "DATE_TIME")
+	private DateTime dateTime;
+	@Column(name = "CREDIT")
+	private double credit;
+	@Column(name = "DEBIT")
+	private double debit;
+	@ManyToOne
+	@JoinColumn(name = "ACCOUNT_ID")
+	private Account account;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public TransactionType getType() {
+		return type;
+	}
+
+	public void setType(TransactionType type) {
+		this.type = type;
+	}
+
+	public DateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(DateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public double getCredit() {
+		return credit;
+	}
+
+	public void setCredit(double credit) {
+		this.credit = credit;
+	}
+
+	public double getDebit() {
+		return debit;
+	}
+
+	public void setDebit(double debit) {
+		this.debit = debit;
+	}
+}
