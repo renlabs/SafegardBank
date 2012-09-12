3a0ce9b660722106544801e587d0e04293a16376
lching <lching@lching.(none)>
2012-09-12 16:49:53 +0800

    dfsfd

--- src/main/java/com/safegard/bank/model/AccountHolder.java
+++ src/main/java/com/safegard/bank/model/AccountHolder.java
@@ -3,9 +3,14 @@ package com.safegard.bank.model;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
+
+@Entity
 @Table(name = "ACCOUNT_HOLDER")
+@XmlRootElement
 public class AccountHolder {
 
     private String lastName;
